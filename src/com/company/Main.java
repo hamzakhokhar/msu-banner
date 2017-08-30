2ecae1f74f1b017b67a4fc21c5895a76034cb1df
Thien Ngo N Le <31322960+thienngole@users.noreply.github.com>
2017-08-29 22:31:40 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Change");
+        System.out.println("Hello guys! ThienNgo was here.");
     }
 }
